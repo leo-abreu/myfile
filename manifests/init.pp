4913be8c8bcf7d3cf4a392234578c549921464f0
Leo Abreu <leo.abreu@puppet.com>
2020-03-23 22:49:28 -0400

    added variable $msg_body

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 class myfile {
+  $msg_body = "default content for myfile\n"
   file {'/root/myfile':
     ensure => present,
-    content => 'default content for myfile',
+    content => $msg_body,
   }
 }
