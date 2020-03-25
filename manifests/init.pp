71bf3c970f3c11ce84d376f73c7bb24b753e6a60
Leo Abreu <leo.abreu@puppet.com>
2020-03-25 11:33:26 -0400

    set owner to root

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 class myfile {
   $msg_body = lookup(myfile::msg_body, undef, undef, "default content for myfile\n\n")
   file {'/tmp/myfile':
-    owner   => centos,
+    owner   => root,
     ensure  => present,
     content => $msg_body,
   }
