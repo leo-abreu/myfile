e230eb776aa2d613df6636b7a57ae9aa6d0fb037
Leo Abreu <leo.abreu@puppet.com>
2020-03-24 23:04:32 -0400

    change ownership

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 class myfile {
   $msg_body = lookup(myfile::msg_body, undef, undef, "default content for myfile\n\n")
   file {'/tmp/myfile':
+    owner   => centos,
     ensure  => present,
     content => $msg_body,
   }
