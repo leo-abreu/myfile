b56fc06f84629574464cdd5b112abfeaf4b135c8
leo-abreu <46694888+leo-abreu@users.noreply.github.com>
2020-04-06 23:09:20 -0400

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 class myfile {
-  $msg_body = lookup(myfile::msg_body, undef, undef, "default content for myfile\n\n")
+  $msg_body = lookup(myfile::msg_body, undef, undef, "default content for myfile \n\n")
   file {'/tmp/myfile':
     owner   => root,
     ensure  => present,
